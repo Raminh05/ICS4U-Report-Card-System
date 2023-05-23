53489e639de72e630f611e10b8c82e8ada67bf4b
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-05-23 16:32:22 -0400

    Add proper class inheritence

--- Student.py
+++ Student.py
@@ -1,19 +1,17 @@
 # -- Class to store student data -- #
 class Person():
-    def __init__(self, firstname: str, lastname: str):
+    def __init__(self, firstname: str, lastname: str) -> None:
         self.firstname = firstname
         self.lastname = lastname
 
 class Student(Person):
-    def __init__(self, student_id: int, course: str, grade: int, assignments: dict) -> None:
-        super().__init__()
+    def __init__(self, firstname: str, lastname: str, student_id: int, course: str, grade: int, assignments: dict) -> None:
+        super().__init__(firstname, lastname)
         self.student_id = student_id
-        self.firstname = firstname
-        self.surname = surname
         self.course = course
         self.grade = grade
         self.assignments = assignments
 
-
         # json stuff here
+
 
