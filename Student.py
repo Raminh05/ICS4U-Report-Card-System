36646ce030b7d2cfdf7c465c326cb5eeeead5205
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-05-23 17:01:41 -0400

    Updated superclass and subclass strcuture

--- Student.py
+++ Student.py
@@ -1,17 +1,28 @@
 # -- Class to store student data -- #
-class Person():
-    def __init__(self, firstname: str, lastname: str) -> None:
-        self.firstname = firstname
-        self.lastname = lastname
+class Course():
+    def __init__(self, course_code: str, course_name: str):
+        self.course_code = course_code
+        self.course_name = course_name
 
 class Student(Person):
-    def __init__(self, firstname: str, lastname: str, student_id: int, course: str, grade: int, assignments: dict) -> None:
-        super().__init__(firstname, lastname)
+    def __init__(self, course_code: str, course_name: str, firstname: str, lastname: str, student_id: int, mark: int, assignments: dict) -> None:
+        super().__init__(course_code, course_name)
+        self.firstname = firstname
+        self.lastname = lastname
         self.student_id = student_id
-        self.course = course
-        self.grade = grade
+        self.mark = mark
         self.assignments = assignments
+    
+    # Temp method to represent the characteristics of a student object
+    def __str__(self):
+        return f'{self.firstname} {self.lastname} with student ID {self.student_id} currently has a {self.mark} in the course.'
+    
+    # call json methods here
 
-        # json stuff here
+
+
+
+
+
 
 
