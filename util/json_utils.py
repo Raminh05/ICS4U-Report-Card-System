9123873758bd01fa0cea76064f3b26c7e3be858e
hallkelv425 <134394510+hallkelv425@users.noreply.github.com>
2023-05-23 17:13:11 -0400

    Add initial json_utils methods

--- util/json_utils.py
+++ util/json_utils.py
@@ -1,3 +1,12 @@
-def json_utils():
-    pass
+import json
+# master_dict is main file with all nested information
+# it is the one being written to a json when report card is saved
+# currently all file paths assume json_utils.py in same folder as student_file.json
+def write_data():
+	json_dict = json.dumps(master_dict, indent = 4)
+	with open("student_file.json", "w") as outfile:
+	outfile.write(json_dict)
 
+def load_data():
+	with open("student_file.json", "r") as infile:
+	master_dict = json.load(infile)
