fbd86b8eae2a1ba45805cd2d2a5eaefb7981b46b
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-05-25 08:26:23 -0400

    Stuff(?)
    - Make script for sorts
    - Update student constructor to auto-write new students to json database
    - Close the file readers in the jsontools
    - Upload dev json database

--- src/JsonTools.py
+++ src/JsonTools.py
@@ -2,14 +2,15 @@ import json
 # master_dict is main file with all nested information
 # it is the one being written to a json when report card is saved
 # currently all file paths assume json_utils.py in same folder as student_file.json
+
 def write_data(master_dict):
 	json_dict = json.dumps(master_dict, indent = 4)
-	with open("student_file.json", "w") as outfile:
+	with open("data//student_file.json", "w") as outfile:
 		outfile.write(json_dict)
 		outfile.close()
 
 def load_data() -> dict:
-	with open("student_file.json", "r") as infile:
+	with open("data//student_file.json", "r") as infile:
 		master_dict = json.load(infile)
 		infile.close()
 	return master_dict
