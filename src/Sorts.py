844e63036ee12fd14b07154b3a9da6ae7071d5f4
hallkelv425 <134394510+hallkelv425@users.noreply.github.com>
2023-05-25 09:47:51 -0400

    Update Sorts.py

--- src/Sorts.py
+++ src/Sorts.py
@@ -2,6 +2,7 @@
 master_dict dictionary contains all information imported from JSON
 selected_student will be a string containing first and last name of student selected in UI
 selected_course will be a string containing course code of course selected in UI
+sort_reverse will be a boolean switched in the UI to sort data in reverse or not
 
 Dictionary is nested as such: master_dict will contain multiple dictionaries for each course (named by their course code) which can be dynamically added through UI
 Each course dictionary will have x number of individual student dictionaries (named by their first and last name) which can also be dynamically added through UI
@@ -12,6 +13,7 @@ This file contains various sort methods for:
 	- Sorting by all students' average class mark
 	- Sorting individual student's assignment marks
 """
+from operator import getitem
 
 selected_course = "ENG4U"
 selected_student = "Kelvin Hall"
@@ -83,8 +85,11 @@ master_dict = {
 	}
 }
 
-print(sorted(master_dict[selected_course][selected_student]["Student marks"].items(), key = lambda x: x[1])) # Sorts selected student's marks in ascending order
 
-print(sorted(master_dict[selected_course][selected_student]["Student marks"].items(), key = lambda x: x[1], reverse = True)) # Sorts selected student's marks in descending order
+print(sorted(master_dict[selected_course][selected_student]["Student marks"].items(), key = lambda x: x[1], reverse = sort_reverse)) # Sorts selected student's marks
 
-print(sorted(master_dict[selected_course].items(), key = lambda x: x[1])) # Sorts selected course's students in alphabetically ascending order
+
+print(sorted(master_dict[selected_course].items(), key = lambda x: getitem(x[1], "Last name"), reverse = sort_reverse)) # Sorts selected course's students by last name
+
+
+print(sorted(master_dict[selected_course].items(), key = lambda x: getitem(x[1], "First name"), reverse = sort_reverse)) # Sorts selected course's students by first name
