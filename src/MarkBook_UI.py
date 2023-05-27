b34ef872228d671bde7a7f343d619f1167969f32
Superior-Taco <nwrhellems@gmail.com>
2023-05-27 00:43:14 -0400

    Update MarkBook_UI.py
    can't figure out the second window resorting to default

--- src/MarkBook_UI.py
+++ src/MarkBook_UI.py
@@ -1,21 +1,30 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QTableView, QTableWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLabel, QTableView, QTableWidget, QTableWidgetItem
 import sys
+from Student import Student
 #Allows for command line arguments
 app = QApplication(sys.argv)
 #Class for the main window
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        #Set the window title
+    #Set the window title
         self.setWindowTitle("Temporary")
-        #Making the Table
-        tableWidget = QTableWidget(12, 3)
-        #Return to class selection
+    #Making the Table
+        tableWidget = QTableWidget(10, 4)
+    #Return to class selection
         exitButton = QPushButton("Return to class selection")
-        #Class information
+    #Delete buttons
+        deleteButton = QPushButton("Delete Student")
+    #View buttons
+        viewButton = QPushButton("View Student")
+    #Student name
+        studentName = QTableWidgetItem("Temp Student name")
+    #Student Grade
+        studentGrade = QTableWidgetItem("50" + "%")
+    #Class information
         classCode = QLabel("temp class code")
         classAverage = QLabel("temp average")
-        #Layout of the page
+    #Layout of the page
         primaryLayout = QVBoxLayout()
         secondaryLayout = QHBoxLayout()
         secondaryLayout.addWidget(classCode)
@@ -23,12 +32,37 @@ class MainWindow(QMainWindow):
         secondaryLayout.addWidget(exitButton)
         primaryLayout.addLayout(secondaryLayout)
         primaryLayout.addWidget(tableWidget)
-        #A widget must exist to create the window
+    #Adding delete buttons
+        tableWidget.setCellWidget(0, 0, deleteButton)
+    #Adding View Buttons
+        tableWidget.setCellWidget(0, 1, viewButton)
+    #Adding student name
+        tableWidget.setItem(0, 2, studentName)
+    #Adding student average
+        tableWidget.setItem(0, 3, studentGrade)
+    #Resizing the table to fit names
+        tableWidget.resizeColumnToContents(2)
+    #Making the student view pop-up appear
+        viewButton.clicked.connect(self.studentWindow)
+    #A widget must exist to create the window
         widget = QWidget()
-        #Adding layout to the widget
+    #Adding layout to the widget
         widget.setLayout(primaryLayout)
-        #Declaring it as the main widget
+    #Declaring it as the main widget
         self.setCentralWidget(widget)
+    #Showing the student view
+    def studentWindow(self, checked):
+        self.w2 = studentView()
+        self.w2.show()
+#Student view window
+class studentView(QWidget):
+    def __innit__(self):
+        super().__innit__()
+        self.setWindowTitle("Student View")
+        layout = QVBoxLayout()
+        presentLabel = QLabel("OH My GOD")
+        layout.addWidget(presentLabel)
+        self.setLayout(layout)
 
 #Windows are hidden by default and must be told to show up
 w = MainWindow()
