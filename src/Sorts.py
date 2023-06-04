0c6e6e594a4332bcb1239c10ddffd67a21e5b89f
hallkelv425 <134394510+hallkelv425@users.noreply.github.com>
2023-06-04 17:23:15 -0400

    Update Sorts.py

--- src/Sorts.py
+++ src/Sorts.py
@@ -16,83 +16,11 @@ This file contains various sort methods for:
 from operator import getitem
 from statistics import mean, median
 
-selected_course = "ENG4U"
-selected_student = "Kelvin Hall"
+def sort_marks(selected_course, selected_student) -> list:
+	sorted(master_dict[selected_course]["Students"][selected_student]["Student marks"].items(), key = lambda x: x[1], reverse = sort_reverse) # Sorts selected student's marks
 
-master_dict = {
-	"ICS4U1": {
-		"Kelvin Hall": {
-			"Student ID": 362981425,
-			"First name": "Kelvin",
-			"Last name": "Hall",
-			"Grade": 12,
-			"Student marks": {
-				"first_assignment": 97,
-				"second_assignment": 91
-			}
-		},
-		"Minh Nguyen": {
-			"Student ID": 363084575,
-			"First name": "Minh",
-			"Last name": "Nguyen",
-			"Grade": 12,
-			"Student marks": {
-				"first_assignment": 78,
-				"second_assignment": 81
-			}
-		},
-		"Nathan Hellems": {
-			"Student ID": 339354771,
-			"First name": "Nathan",
-			"Last name": "Hellems",
-			"Grade": 12,
-			"Student marks": {
-				"first_assignment": 88,
-				"second_assignment": 90
-			}
-		}
-	},
-	"ENG4U": {
-		"Kelvin Hall": {
-			"Student ID": 362981425,
-			"First name": "Kelvin",
-			"Last name": "Hall",
-			"Grade": 12,
-			"Student marks": {
-				"first_assignment": 107,
-				"second_assignment": 102,
-				"third_assignment": 69
-			}
-		},
-		"Minh Nguyen": {
-			"Student ID": 363084575,
-			"First name": "Minh",
-			"Last name": "Nguyen",
-			"Grade": 12,
-			"Student marks": {
-				"first_assignment": 68,
-				"second_assignment": 73
-			}
-		},
-		"Nathan Hellems": {
-			"Student ID": 339354771,
-			"First name": "Nathan",
-			"Last name": "Hellems",
-			"Grade": 12,
-			"Student marks": {
-				"first_assignment": 93,
-				"second_assignment": 79
-			}
-		}
-	}
-}
+def sort_lname(selected_course) -> list:
+	sorted(master_dict[selected_course]["Students"].items(), key = lambda x: getitem(x[1], "Last name"), reverse = sort_reverse) # Sorts selected course's students by last name
 
-print(sorted(master_dict[selected_course][selected_student]["Student marks"].items(), key = lambda x: x[1], reverse = sort_reverse)) # Sorts selected student's marks
-
-print(sorted(master_dict[selected_course].items(), key = lambda x: getitem(x[1], "Last name"), reverse = sort_reverse)) # Sorts selected course's students by last name
-
-print(sorted(master_dict[selected_course].items(), key = lambda x: getitem(x[1], "First name"), reverse = sort_reverse)) # Sorts selected course's students by first name
-
-print(mean(master_dict[selected_course][selected_student]["Student marks"].values())) # Calculates average mark for selected student
-
-print(median(master_dict[selected_course][selected_student]["Student marks"].values())) # Calculates median mark for selected student
+def sort_fname(selected_course) -> list:
+	sorted(master_dict[selected_course]["Students"].items(), key = lambda x: getitem(x[1], "First name"), reverse = sort_reverse) # Sorts selected course's students by first name
