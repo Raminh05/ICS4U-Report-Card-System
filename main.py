1588d766842dfb9c0be2e8da71b52e1c4b2ef309
hallkelv425 <134394510+hallkelv425@users.noreply.github.com>
2023-06-05 09:26:14 -0400

    Rewriting sorts

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from src.Views import *
 from src.Course import Course
 import src.Sorts
 
-
 master_dict = decrypt_json(load_data())
 clearConsole()
 
 def main():
-	selected_class = class_selection(master_dict)
 	course_table(selected_class)
 	selected_class.class_view_action_list()
 	print(selected_class)
--- src/Sorts.py
+++ src/Sorts.py
@@ -1,26 +1,11 @@
-"""
-master_dict dictionary contains all information imported from JSON
-selected_student will be a string containing first and last name of student selected in UI
-selected_course will be a string containing course code of course selected in UI
-sort_reverse will be a boolean switched in the UI to sort data in reverse or not
+def mark_sort(reverse_sort):
+	master_dict["ICS4U"].students = sorted(master_dict["ICS4U"].students, key=lambda student: student.mark, reverse = reverse_sort)
+	selected_class = class_selection(master_dict)
 
-Dictionary is nested as such: master_dict will contain multiple dictionaries for each course (named by their course code) which can be dynamically added through UI
-Each course dictionary will have x number of individual student dictionaries (named by their first and last name) which can also be dynamically added through UI
-Each student dictionary will contain misc student info (first/last name, Grade, etc) as well as another dictionary with all their assignments and respective marks (can be dynamically added through UI)
+def firstname_sort():
+	master_dict["ICS4U"].students = sorted(master_dict["ICS4U"].students, key=lambda student: student.firstname, reverse = reverse_sort)
+	selected_class = class_selection(master_dict)
 
-This file contains various sort methods for:
-	- Sorting by first or last name alphabetically
-	- Sorting by all students' average class mark
-	- Sorting individual student's assignment marks
-"""
-from operator import getitem
-from statistics import mean, median
-
-def sort_marks(selected_course, selected_student) -> list:
-	sorted(master_dict[selected_course]["Students"][selected_student]["Student marks"].items(), key = lambda x: x[1], reverse = sort_reverse) # Sorts selected student's marks
-
-def sort_lname(selected_course) -> list:
-	sorted(master_dict[selected_course]["Students"].items(), key = lambda x: getitem(x[1], "Last name"), reverse = sort_reverse) # Sorts selected course's students by last name
-
-def sort_fname(selected_course) -> list:
-	sorted(master_dict[selected_course]["Students"].items(), key = lambda x: getitem(x[1], "First name"), reverse = sort_reverse) # Sorts selected course's students by first name
+def lastname_sort():
+	master_dict["ICS4U"].students = sorted(master_dict["ICS4U"].students, key=lambda student: student.lastname, reverse = reverse_sort)
+	selected_class = class_selection(master_dict)
