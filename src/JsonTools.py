cc7162fda9c4f9226e8fd27a8e6c409f5106ae7d
Minh Nguyen <77513059+Raminh05@users.noreply.github.com>
2023-06-04 20:11:21 -0400

    Trying again to make cross-platform directories

--- src/JsonTools.py
+++ src/JsonTools.py
@@ -5,11 +5,15 @@ import json, os
 # currently all file paths assume json_utils.py in same folder as student_file.json
 
 # Making sure that the path to the data folder works on all OSes by using os module to assemble paths
-joined_database_path = os.path.join("data", "student_file.json")
+
+if os.name == "nt":
+	joined_database_path = "\\data\\student_file.json"
+else:
+	joined_database_path = "/data/student_file.json"
 
 def load_data() -> dict:
 	""" Reading all data from the json database and packaging it into a python-readable dictionary """
-	with open(f"{os.getcwd()}/{joined_database_path}", "r") as infile:
+	with open(f"{os.getcwd()}{joined_database_path}", "r") as infile:
 		master_dict = json.load(infile)
 		infile.close()
 	return master_dict
@@ -18,7 +22,7 @@ def write_data(master_dict: dict) -> None:
 	""" Writing the master dictionary containing all classes and students into the json database """
 	json_dict = json.dumps(master_dict, indent = 4)
 	try:
-		with open(f"{os.getcwd()}/{joined_database_path}", "w") as outfile:
+		with open(f"{os.getcwd()}{joined_database_path}", "w") as outfile:
 			outfile.write(json_dict)
 			outfile.close()
 	except OSError:
