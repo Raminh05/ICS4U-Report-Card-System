ec3307b08edea982a8189e1bed73e5ec601de453
hallkelv425 <134394510+hallkelv425@users.noreply.github.com>
2023-06-04 22:21:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,18 +10,34 @@ def clearConsole(): # Generic function to clear output console before running
     elif osType == "Windows":
         os.system("cls")
 
-def main():
-	clearConsole()
-	master_dict = decrypt_json(load_data())
-	table = texttable.Texttable()
-	table.set_deco(table.HEADER | table.HLINES)
-	table.set_cols_dtype(["i", "t", "t", "t"])
-	table.set_cols_align(["c", "c", "c", "c"])
+clearConsole()
+table = texttable.Texttable()
+master_dict = decrypt_json(load_data())
+table.set_deco(table.HEADER | table.HLINES | table.VLINES | table.BORDER)
+
+def course_table():
+	table.reset()
+	table.set_cols_dtype(["t", "t", "t", "t"])
 	table.set_cols_align(["c", "c", "c", "c"])
 	table.header(["Student ID", "First name", "Last name", "Course average"])
-	for student in master_dict["ICS4U"].students:
+	for student in master_dict["ICS4U"].students: # temporarily hardcoded course code
 		table.add_row([student.id, student.firstname, student.lastname, student.mark])
 	print(table.draw())
+
+def individual_student_table():
+	table.reset()
+	table.set_cols_dtype(["t", "t"])
+	table.set_cols_align(["c", "c"])
+	table.set_cols_valign(["m", "m"])
+	table.header(["Assignment name", "Mark"])
+	assignments = master_dict["ICS4U"].students[0].assignments
+	for key in list(assignments.keys()):
+		table.add_row([key, assignments[key]])
+	print(table.draw())
+
+def main():
+	course_table()
+	individual_student_table()
 
 if __name__ == "__main__":
 	main()
