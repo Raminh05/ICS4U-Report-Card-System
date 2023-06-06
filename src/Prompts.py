16baf51c364de7f879336a8cd2611d8b15a2b819
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-06-06 13:05:57 -0400

    Move class_view_action_list out of Prompts.py backto Course.py

--- src/Prompts.py
+++ src/Prompts.py
@@ -14,27 +14,9 @@ def class_selection(master_dict):
 
 	return master_dict[selected_class]
 
-def class_view_action_list(selected_course):
-		print("1. Add New Student\n2. View/Edit Student\n3. Delete Student From Class\n4. Return to class selection\n5. Quit Program")
 
-		selected_action = inquirer.number(
-				message="What action do you want to do? (Enter in displayed list number)",
-				min_allowed = 1,
-				max_allowed = 5,
-				validate = EmptyInputValidator(),
-			).execute()
-		
-		match selected_action:
-			case "1":
-				selected_course.add_student()
-			case "2":
-				selected_course.view_edit_student()
-			case "3":
-				selected_course.remove_student()
-			case "4":
-				return "4"
-			case "5":
-				quit()
+
+
 	
 
 
