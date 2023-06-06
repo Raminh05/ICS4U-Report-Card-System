c8cb5cc8b3fcd7f3c14246de2be77e8c16a07bd5
hallkelv425 <134394510+hallkelv425@users.noreply.github.com>
2023-06-06 08:22:49 -0400

    Update Sorts.py

--- src/Sorts.py
+++ src/Sorts.py
@@ -1,11 +1,11 @@
 def mark_sort(reverse_sort):
-	master_dict["ICS4U"].students = sorted(master_dict["ICS4U"].students, key=lambda student: student.mark, reverse = reverse_sort)
-	selected_class = class_selection(master_dict)
+	master_dict["ICS4U"].students.sort(key=lambda student: student.mark, reverse = reverse_sort)
 
 def firstname_sort():
-	master_dict["ICS4U"].students = sorted(master_dict["ICS4U"].students, key=lambda student: student.firstname, reverse = reverse_sort)
-	selected_class = class_selection(master_dict)
+	master_dict["ICS4U"].students.sort(key=lambda student: student.firstname, reverse = reverse_sort)
 
 def lastname_sort():
-	master_dict["ICS4U"].students = sorted(master_dict["ICS4U"].students, key=lambda student: student.lastname, reverse = reverse_sort)
-	selected_class = class_selection(master_dict)
+	master_dict["ICS4U"].students.sort(key=lambda student: student.firstname, reverse = reverse_sort)
+
+# def marks_sort(selected_student):
+# 	master_dict["ICS4U"].students[selected_student].assignments.values().sort(key=lambda student: student.lastname, reverse = reverse_sort)
