899a2bb6e57533aaef829966586590db6b5bd0f9
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-06-06 21:43:00 -0400

    Updated main program event

--- main.py
+++ main.py
@@ -1,17 +1,33 @@
-from src.JsonTools import load_data
-from src.DecryptJson import decrypt_json
 from src.Prompts import *
 from src.Views import *
 import src.Sorts
+from src.Course import Course
+from src.People import Teacher
 
 def main():
+	""" Main program event """
 	clearConsole()
-	master_dict = decrypt_json(load_data())
-	selected_class = class_selection(master_dict)
-	course_table(selected_class)
+	selected_class = class_selection()
 
-	if selected_class.class_view_action_list() == "4":
+	# If entered in course code does not exist in the database (code 1)
+	if selected_class[0] == 1:
+		if make_new_class():
+			course_name = input("What is the name of the course? ")
+			teacher_first_name = input("What is the first name of the teacher? ")
+			teacher_last_name = input("What is the last name of the teacher? ")
+			confirmed_class = Course(selected_class[1], course_name, Teacher(teacher_first_name, teacher_last_name), []) # Makes new course object
+		else:
+			main()
+
+	# If the course code does exist (code 0), confirmed_class IS the class object straight from the dictionary
+	else:
+		confirmed_class = selected_class[1]
+
+	course_table(confirmed_class)
+
+	if confirmed_class.class_view_action_list() == "4":
+		confirmed_class.write_to_json()
 		main()
-
+	
 if __name__ == "__main__":
 	main()
--- src/Views.py
+++ src/Views.py
@@ -2,26 +2,28 @@ import texttable, platform, os
 from src.Prompts import *
 
 # Generic function to clear output console before running
-def clearConsole(): 
+def clearConsole() -> None: 
     osType = platform.system()
     if osType == "Darwin" or osType == "Linux":
         os.system("clear")
     elif osType == "Windows":
         os.system("cls")
 
-def course_table(selected_course):
+# Renders the view to display all information about a specific course
+def course_table(selected_course) -> None:
 	clearConsole()
-	print("Course name: "+selected_course.course_name+"\nCourse Code: "+selected_course.course_code+"\nTeacher: "+str(selected_course.teacher)+"\nCourse average: "+str(selected_course.average))
+	print("Course name: "+selected_course.course_name+"\nCourse Code: "+selected_course.course_code+"\nTeacher: "+str(selected_course.teacher)+"\nCourse average: "+str(selected_course.average)+"%")
 	table = texttable.Texttable()
 	table.set_cols_dtype(["t", "t", "t", "t", "t"])
 	table.set_cols_align(["c", "c", "c", "c", "c"])
 	table.set_cols_valign(["m", "m", "m", "m", "m"])
 	table.header(["", "Student ID", "First name", "Last name", "Student average"])
 	for count, student in enumerate(selected_course.students):
-		table.add_row([count, student.id, student.firstname, student.lastname, student.mark])
+		table.add_row([count, student.id, student.firstname, student.lastname, f"{student.mark}%"])
 	print(table.draw())
-	
-def individual_student_table(student):
+
+# Renders the view to display all information about a specific student
+def individual_student_table(student) -> None:
 	clearConsole()
 	print("Student Name: " + student.firstname + " " + student.lastname + "\n" + "Student Average: " + str(student.mark) + "%")
 	table = texttable.Texttable()
@@ -31,5 +33,5 @@ def individual_student_table(student):
 	table.header(["Assignment", "Mark"])
 	assignments = student.assignments
 	for key in list(assignments.keys()):
-		table.add_row([key, assignments[key]])
+		table.add_row([key, str(assignments[key]) + "%"])
 	print(table.draw())
