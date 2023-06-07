b843dd6563d818e81a3d5de8164d8e23a5cafb2b
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-06-06 21:46:45 -0400

    Massive overhaul to Course.py
    way too many things to explain lol

--- src/Course.py
+++ src/Course.py
@@ -1,6 +1,6 @@
 from src.People import Teacher, Student
-from statistics import mean, median
-from src.JsonTools import write_data, load_data
+from statistics import mean
+from src.JsonTools import *
 from InquirerPy import inquirer
 from InquirerPy.validator import EmptyInputValidator
 from src.Views import individual_student_table, course_table
@@ -8,6 +8,14 @@ from src.Views import individual_student_table, course_table
 # -- Class to store course data -- #
 class Course():
 	def __init__(self, course_code: str, course_name: str, teacher: Teacher, students: list):
+		"""Constructor for the Course class
+
+		Args:
+			course_code (str): The identification code of the course
+			course_name (str): The string literal name of the course
+			teacher (Teacher): The teacher of the course represented by a Teacher object
+			students (list): A list of student objects that attend the course
+		"""
 		self.course_code = course_code.upper()
 		self.course_name = course_name
 		self.teacher = teacher
@@ -16,17 +24,21 @@ class Course():
 		self.class_average()
 
 	def __repr__(self):
+		""" Provides a string representation of a course object"""
 		return f'Course({self.course_code}, {self.course_name}, {self.teacher}, {self.students})'
 
-	# I tried to convert the entirety of the course class into a dictionary :skull:
+	# I tried to convert the entirety of the course class into a dictionary (so that I can write it into the json) :skull: (__dict__ doesn't work)
 	def write_to_json(self) -> None:
+		# Before writing class to json, update class average
+		self.class_average() 
 		master_dict = load_data()
 		course_dict = master_dict[self.course_code] = {}
 		course_dict["Course name"] = self.course_name
 		course_dict["Teacher"] = f'{self.teacher}'
 		course_dict["Class average"] = self.average
 		students_dict = course_dict["Students"] = {}
-
+		
+		# Goes through all the student objects in the Course.students and turns them into sub-dictionaries for the json
 		for student in self.students:
 			student_dict = students_dict[f"{student.firstname} {student.lastname}"] = {}
 			student_dict["First name"] = student.firstname
@@ -36,51 +48,64 @@ class Course():
 			student_dict["Student marks"] = student.assignments
 
 		write_data(master_dict)
-		print("Wrote information into database!")
-	
+
 	def remove_from_json(self) -> None:
+		"""Remove course from json database"""
 		master_dict = load_data()
 		del master_dict[self.course_code]
 		write_data(master_dict)
 	
 	def add_student(self) -> None:
-		firstname = input("Enter Student's First Name: ")
-		lastname = input("Enter Student's Last Name: ")
-		id = int(input("Enter Student's ID: "))
+		"""Add new student object to student list (Course.students) of the course"""
+		firstname = inquirer.text(message = "Enter Student's First Name: ").execute()
+		lastname = inquirer.text(message = "Enter Student's Last Name: ").execute()
+		id = int(inquirer.text(message = "Enter Student's ID: ").execute())
 		self.students.append(Student(firstname, lastname, id, {}))
-		self.class_average()
 	
 	def view_edit_student(self) -> None:
-		index = int(input("What student do you want to view/edit? (Enter in student's index in the table): "))
-		selected_student = self.students[index]
-		individual_student_table(selected_student)
-
-		print("1. Add Assignment\n2. Remove Assignment\n3. Edit assignment\n4. Edit student info\n5. Return to class view")
-		selected_action = inquirer.number(
-			message = "What action do you want to do?",
-			min_allowed = 1,
-			max_allowed = 5,
-			validate = EmptyInputValidator()
+		"""Calls out the student information table to be rendered and provides action list for student view"""
+		index = inquirer.number(
+			message = "What student do you want to view/edit? (Enter in student's index in the table): ",
+			min_allowed = 0,
+			max_allowed = len(self.students) - 1,
+			validate = EmptyInputValidator(),
 		).execute()
 
+		selected_student = self.students[int(index)]
+		individual_student_table(selected_student) # Renders table to display individual student information
+
+		action = inquirer.select(
+		message="Select an action:",
+		choices=[
+			"Add Assignment",
+			"Remove Assignment",
+			"Edit Assignment Mark",
+			"Edit Student Info",
+			"Return to Class View",
+		],
+		default=None,
+	).execute()
+
 		# Return "5" is the status code for this match case to return to the class selection view, code 5 is passed into the class_list_action_view to trigger code 4 from there (yes the code is stupid but hey it works)
-		match selected_action:
-			case "1":
-				assignment_name = input("Enter in assignment name: ")
-				assignment_mark = int(input("Enter in assignment mark: "))
+		match action:
+			case "Add Assignment":
+				assignment_name = inquirer.text(message = "Enter in assignment name: ").execute()
+				assignment_mark = int(inquirer.text(message = "Enter in assignment mark: ").execute())
 				selected_student.add_assignment(assignment_name, assignment_mark)
-				return "5"
-			case "4":
-				firstname = input("Enter Student's First Name: ")
-				lastname = input("Enter Student's Last Name: ")
-				id = int(input("Enter Student's ID: "))
+				
+			case "Edit Student Info":
+				firstname = inquirer.text(message = "Enter Student's First Name: ").execute()
+				lastname = inquirer.text(message = "Enter Student's Last Name: ").execute()
+				id = int(inquirer.text(message = "Enter Student's ID: ").execute())
 
 				selected_student.firstname = firstname
 				selected_student.lastname = lastname
 				selected_student.id = id
-				return "5"
-			case "5":
-				return "5"
+
+			case "Return to Class View":
+				course_table(self)
+				self.class_view_action_list()
+
 			case _:
 				selectables = {}
 				for key in list(selected_student.assignments.keys()):
@@ -92,53 +117,66 @@ class Course():
 					multicolumn_complete = True,
 				).execute()
 
-				if selected_action == "2":
+				if action == "Remove Assignment":
 					selected_student.remove_assignment(assignment_name)
 					
-				elif selected_action == "3":
+				elif action == "Edit Assignment Mark":
 					selected_student.assignments[assignment_name] = int(input(f"Enter in the new mark for {assignment_name}: "))
-				return "5"
+				
+		individual_student_table(selected_student)
 		
-			
 	def class_view_action_list(self):
-		print("\n1. Add New Student\n2. View/Edit Student\n3. Delete Student From Class\n4. Return to class selection\n5. Save to database\n6. Quit Program")
-
-		selected_action = inquirer.number(
-				message="What action do you want to do? (Enter in displayed list number)",
-				min_allowed = 1,
-				max_allowed = 6,
-				validate = EmptyInputValidator(),
-			).execute()
+		"""Action list for the student information view"""
+		action = inquirer.select(
+        message="Select an action:",
+        choices=[
+            "Add New Student",
+            "View/Edit Student",
+	    	"Delete Student From Class",
+		    "Add an Assignment to all Students in Class",
+		    "Return to Class Selection",
+		    "Manually Save to Database",
+            "Exit",
+        ],
+        default=None,
+    ).execute()
 		
 		# Return "4" is a status code that tells the program to go back to class selection view
-		match selected_action:
-			case "1":
+		match action:
+			case "Add New Student":
 				self.add_student()
-				return "4"
-			case "2":
-				if self.view_edit_student() == "5":
+			case "View/Edit Student":
+					self.view_edit_student()
 					self.write_to_json()
-					return "4"
-				else:
-					self.write_to_json()
-			case "3":
+			case "Delete Student From Class":
 				self.remove_student()
+			case "Add an Assignment to all Students in Class":
+				self.add_assignment()
+			case "Return to Class Selection":
 				return "4"
-			case "4":
-				return "4"
-			case "5":
+			case "Manually Save to Database":
 				self.write_to_json()
-				return "4"
-			case "6":
+			case "Exit":
+				self.write_to_json()
 				quit()
-	
+		
+		course_table(self)
+		self.class_view_action_list()
+			
 	def remove_student(self) -> None:
-		index = int(input("What student do you want to remove? (Enter in student's index in the table): "))
-		self.students.remove(self.students[index])
-		self.class_average()
+		"""Removes student object from the course object"""
+		index = inquirer.number(
+			message = "What student do you want to remove? (Enter in student's index in the table): ",
+			min_allowed = 0,
+			max_allowed = len(self.students) - 1,
+			validate = EmptyInputValidator(),
+		).execute(0)
 
+		self.students.remove(self.students[int(index)])
+		
 	# Add an assignment to ALL students' assignment dictionary
-	def add_assignment(self, assignment_name: str) -> None:
+	def add_assignment(self) -> None:
+		assignment_name = inquirer.text(message = "What is the name of the new assignment?").execute()
 		for student in self.students:
 			student.assignments[assignment_name] = 0
 			student.average()
@@ -166,12 +204,9 @@ class Course():
 	# Calculate class average
 	def class_average(self) -> None:
 		sum = 0
-		for student in self.students:
-			sum += student.mark
-		self.average = round(sum / len(self.students), 1)
-
-		
-
-			
-		
-		
+		if len(self.students) > 0:
+			for student in self.students:
+				sum += student.mark
+			self.average = round(sum / len(self.students), 1)
+		else:
+			self.average = 0
