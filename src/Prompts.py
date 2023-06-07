bbe0e4a06f1c1346f0ed7b67b3b603bef025235a
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-06-06 21:46:11 -0400

    Added checking for unknown course codes
    - Now users are prompted to create a new course if they entered in a course code that does not yet exist in the database

--- src/Prompts.py
+++ src/Prompts.py
@@ -1,7 +1,10 @@
 from InquirerPy import inquirer
-from InquirerPy.validator import EmptyInputValidator
+from src.JsonTools import load_data
+from src.DecryptJson import decrypt_json
 
-def class_selection(master_dict):
+# If code 1 is returned because the course does not yet exist in the database, prompt user to make new course in main.py 
+def class_selection() -> tuple:
+	master_dict = decrypt_json(load_data())
 	selectables = {}
 	for key in list(master_dict.keys()):
 		selectables[key] = None
@@ -12,15 +15,12 @@ def class_selection(master_dict):
 		multicolumn_complete=True,
 	).execute()
 
-	return master_dict[selected_class]
+	if selected_class in master_dict:
+		return 0, master_dict[selected_class]
+	else:
+		return 1, selected_class
 
-
-
-
-	
-
-
-
-
-
-	
+# A function to prompt the user if they want to make a new class
+def make_new_class() -> bool:
+	selection = inquirer.confirm(message="You have entered in a non-existent class! Would you like to make a new one?", default=True).execute()
+	return selection
