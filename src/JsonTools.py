eec467deeedfb476db22725d71149050b8a4eb69
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2023-06-06 21:44:06 -0400

    Improved error handling with JsonTools.py

--- src/JsonTools.py
+++ src/JsonTools.py
@@ -1,4 +1,4 @@
-import json, os
+import json, os, time
 
 # master_dict is main file with all nested information
 # it is the one being written to a json when report card is saved
@@ -13,10 +13,15 @@ else:
 
 def load_data() -> dict:
 	""" Reading all data from the json database and packaging it into a python-readable dictionary """
-	with open(f"{os.getcwd()}{joined_database_path}", "r") as infile:
-		master_dict = json.load(infile)
-		infile.close()
-	return master_dict
+	try:
+		with open(f"{os.getcwd()}{joined_database_path}", "r") as infile:
+			master_dict = json.load(infile)
+			infile.close()
+	except OSError:
+		print("File not found!")
+		return {}
+	else:
+		return master_dict
 	
 def write_data(master_dict: dict) -> None:
 	""" Writing the master dictionary containing all classes and students into the json database """
@@ -25,6 +30,8 @@ def write_data(master_dict: dict) -> None:
 		with open(f"{os.getcwd()}{joined_database_path}", "w") as outfile:
 			outfile.write(json_dict)
 			outfile.close()
+		print("Successfully wrote data to JSON!")
+		time.sleep(1)
 	except OSError:
 		print("File not found!")
 	
